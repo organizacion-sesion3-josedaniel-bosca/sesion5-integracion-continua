cf537cbf8e81cb0d149725b37bb4083021bfd023
Josep Daniel Boscà Candel <162562524+josedanielbosca@users.noreply.github.com>
2024-03-10 17:32:05 +0100

    Update operaciones.py

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -1,5 +1,5 @@
 # En principio el alumno solo recibiría la definición de la función en el archivo correspondiente
-def suma(x,y):
+def par(x):
 
     # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
-    return x + y
+    return x % 2
